e105505d7d9815947b70652ccb19bc5d50e67d68
obaraelijah <elijahobara357@gmail.com>
2024-05-29 21:01:00 +0300

    feat: Run jobs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use std::{num::NonZeroUsize, sync::{mpsc, Condvar, Mutex}};
+use std::{num::NonZeroUsize, sync::{mpsc, Condvar, Mutex}, thread::JoinHandle};
 
 pub struct MultiThread {
     current_thread: bool,
     senders: Vec<mpsc::Sender<Job<'static>>>,
+    handles: Vec<JoinHandle<()>>,
 }
 
 impl MultiThread {
@@ -10,13 +11,43 @@ impl MultiThread {
         let n_threads = n_threads.get();
         let mut result = MultiThread {
             current_thread: false,
-            senders: Vec::with_capacity(n_threads)
+            senders: Vec::with_capacity(n_threads),
+            handles: Vec::with_capacity(n_threads),
         };
+        for _ in 0..n_threads {
+            let (sender, receiver) = mpsc::channel::<Job>();
+            let handle = std::thread::spawn(move || {
+                for job in receiver {
+                    (job.f)()
+                }
+            });
+            result.senders.push(sender);
+            result.handles.push(handle)
+        }
         result
     }
 
     pub fn new_current_thread() -> MultiThread {
-        MultiThread { current_thread: true , senders: Vec::new(), }
+        MultiThread { current_thread: true , senders: Vec::new(), handles: Vec::new() }
+    }
+
+    pub fn run<F>(&self, job: F)
+    where
+        F: Fn() + Sync,
+    {
+        if self.current_thread {
+            job();
+            return;
+        }
+        self.run_par(&job);
+    }
+
+    fn run_par(&self, f: &(dyn Fn() + Sync)) {
+        let job_count = JobCount::new();
+        for s in &self.senders {
+            let job = Job { f, _g: job_count.inc() };
+            s.send(unsafe { job.erase_lifetime() }).unwrap();
+        }
     }
 }
 
